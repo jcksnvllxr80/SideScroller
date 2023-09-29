3443bb6c197350c03c560efe57c6b968b3cf54ed
Aaron Watkins <ac.watkins80@gmail.com>
2023-09-28 23:33:02 -0400

    duplicate level1 tilemap add eagle, opossum, and gem classes and blueprints

---
+++ Source/SideScroller/Characters/Enemies/PC_Enemy_Opossum.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnemyCollisionPaperCharacter.h"
+#include "PC_Enemy_Opossum.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIDESCROLLER_API APC_Enemy_Opossum : public AEnemyCollisionPaperCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SideScroller/Pickups/Gem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePickup.h"
+#include "Gem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIDESCROLLER_API AGem : public ABasePickup
+{
+	GENERATED_BODY()
+	
+};
