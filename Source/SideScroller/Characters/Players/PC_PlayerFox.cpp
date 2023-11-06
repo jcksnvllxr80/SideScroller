926f509591bdf8c4d693e923df68c2de134b8932
Aaron Watkins <ac.watkins80@gmail.com>
2023-11-06 16:33:53 -0500

    add try catch around spectate command

--- Source/SideScroller/Characters/Players/PC_PlayerFox.cpp
+++ Source/SideScroller/Characters/Players/PC_PlayerFox.cpp
@@ -309,15 +309,15 @@ void APC_PlayerFox::Spectate() const
 		return;
 	}
 
-	PlayerController->SetViewTargetWithBlend(SpectableActor);
-	// try
-	// {
-	// 	PlayerController->SetViewTargetWithBlend(SpectableActor);
-	// }
-	// catch (...)
-	// {
-	// 	UE_LOG(LogTemp, Error, TEXT("APC_PlayerFox::Spectate - Catch block - cant SetViewTargetWithBlend"))
-	// }
+	// PlayerController->SetViewTargetWithBlend(SpectableActor);
+	try
+	{
+		PlayerController->SetViewTargetWithBlend(SpectableActor);
+	}
+	catch (...)
+	{
+		UE_LOG(LogTemp, Error, TEXT("APC_PlayerFox::Spectate - Catch block - cant SetViewTargetWithBlend"))
+	}
 }
 
 void APC_PlayerFox::SpectatePrevPlayer()
